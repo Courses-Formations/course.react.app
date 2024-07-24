9fde97dcb81e4047561029e5110ddcebb601bd19
andre.gastao <andregastaocamuenhi@gmail.com>
2024-07-24 20:33:38 +0100

    formação inicial do react js e primeiro commit

---
+++ course-reactapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Minha Aplicação em React JS</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
